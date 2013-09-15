88d9b19dac91e28e086eb0faf9c723c3fb4ed3d3
zltqzj <zltqzj@163.com>
2013-09-15 14:01:28 +0800

    Initial Commit

---
+++ NetAndParse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetAndParse
+//
+//  Created by ZKR on 9/15/13.
+//  Copyright (c) 2013 ZKR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetAndParse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetAndParse
+//
+//  Created by ZKR on 9/15/13.
+//  Copyright (c) 2013 ZKR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
